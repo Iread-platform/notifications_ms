81331dc9b8a85f3127c6c3aeb8664634235955cd
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-09-24 22:51:02 +0300

    Inject Add Table annotations to Notification table.

--- DataAccess/Data/Entity/Notification.cs
+++ DataAccess/Data/Entity/Notification.cs
@@ -1,12 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
 
 namespace iread_notifications_ms.DataAccess.Data.Entity
 {
+    [Table("Notifications")]
     public class Notification
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
         public int Id { get; set; }
         public String Title { get; set; }
+        public DateTime Created { get; set; }
         public String Token { get; set; }
 
         public String Body
@@ -14,8 +23,6 @@ namespace iread_notifications_ms.DataAccess.Data.Entity
             get; set;
         }
         public bool IsSent { get; set; }
-
-        // public Dictionary<String, String> data { get; set; }
     }
 
 }
