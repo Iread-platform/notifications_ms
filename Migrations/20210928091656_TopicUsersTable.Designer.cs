5103f70a19293b927ceb92a2cfd3bd98c6a449f3
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-09-28 12:19:30 +0300

    IR-219 Add migration for the many-to-many relation.

--- Migrations/20210928091656_TopicUsersTable.Designer.cs
+++ Migrations/20210928091656_TopicUsersTable.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using iread_notifications_ms.DataAccess;
 
 namespace iread_notifications_ms.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210928091656_TopicUsersTable")]
+    partial class TopicUsersTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210928091656_TopicUsersTable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace iread_notifications_ms.Migrations
+{
+    public partial class TopicUsersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TopicUsers",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "varchar(767)", nullable: false),
+                    TopicId = table.Column<int>(type: "int", nullable: false),
+                    DevicesToken = table.Column<string>(type: "varchar(767)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TopicUsers", x => new { x.UserId, x.TopicId });
+                    table.ForeignKey(
+                        name: "FK_TopicUsers_Devices_DevicesToken",
+                        column: x => x.DevicesToken,
+                        principalTable: "Devices",
+                        principalColumn: "Token",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TopicUsers_Topics_TopicId",
+                        column: x => x.TopicId,
+                        principalTable: "Topics",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TopicUsers_DevicesToken",
+                table: "TopicUsers",
+                column: "DevicesToken");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TopicUsers_TopicId",
+                table: "TopicUsers",
+                column: "TopicId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TopicUsers");
+        }
+    }
+}
