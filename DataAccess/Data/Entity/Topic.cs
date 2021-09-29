ff418d4c36fa90b6397666acb8211d6405137584
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-09-29 20:14:34 +0300

    IR-219 Set topic naming conversions

--- DataAccess/Data/Entity/Topic.cs
+++ DataAccess/Data/Entity/Topic.cs
@@ -13,6 +13,9 @@ namespace iread_notifications_ms.DataAccess.Data.Entity
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Required]
         public int Id { get; set; }
+
+
+        [RegularExpression(@"[a-zA-Z0-9-_.~%]+")]
         public string Title { get; set; }
         public List<TopicNotification> Notifications { get; set; }
     }
