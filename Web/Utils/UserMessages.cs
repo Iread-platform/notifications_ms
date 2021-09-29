e8dc1dacfae1848379218b854d07207e3bcb0526
Samir_N_Ahmad <samir.ahmad.codavia@gmail.com>
2021-09-29 20:13:04 +0300

    IR-219 Change user messages type to const

--- Web/Utils/UserMessages.cs
+++ Web/Utils/UserMessages.cs
@@ -9,17 +9,19 @@ namespace iread_notifications_ms.Web.Utils
     public class UserMessages
     {
         // Topic messages.
-        public static string TOPIC_TITLE_EXISTS = "A topic with the same title already exists.";
-        public static string TOPIC_NO_SUBSCRIBERS = "This topic has no subscribers.";
-        public static string TOPIC_NO_NOTIFICATIONS = "This topic has no notifications.";
-        public static string TOPIC_NO_FOUND = "There is no topic with such info.";
+        public const string TOPIC_TITLE_EXISTS = "A topic with the same title already exists.";
+        public const string TOPIC_IVALID_NAME = "Invalid topic name, Allowed characters: a-z/A-Z, numbers: 0-9, and . - % .";
+        public const string TOPIC_NO_SUBSCRIBERS = "This topic has no subscribers.";
+        public const string TOPIC_NO_NOTIFICATIONS = "This topic has no notifications.";
+        public const string TOPIC_NO_FOUND = "There is no topic with such info.";
 
         // Notification messages.
-        public static string NOTIFICATION_NOT_SENT = "Could  not deliver this notification.";
+        public const string NOTIFICATION_NOT_SENT = "Could  not deliver this notification.";
+        public const string NOTIFICATION_SENT = "Notification was sent successfully.";
 
         //User messages
-        public static string USER_NO_NOTIFICATIONS = "This user has no notifications.";
-        public static string USER_NO_FOUND = "There is no user with such info.";
+        public const string USER_NO_NOTIFICATIONS = "This user has no notifications.";
+        public const string USER_NO_FOUND = "There is no user with such info.";
         public static List<String> ModelStateParser(ModelStateDictionary modelStateDictionary)
         {
             return modelStateDictionary.Values.SelectMany(x => x.Errors.Select(y => y.ErrorMessage)).ToList();
